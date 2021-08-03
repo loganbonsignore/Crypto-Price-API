0a18f7bdde6daba2a3f9100841bc35d886cafb7f
Logan Bonsignore <Loganbon14@gmail.com>
2021-08-02 23:32:28 -0500

    remove config

--- api/sources/livecryptowatch.py
+++ api/sources/livecryptowatch.py
@@ -4,14 +4,13 @@
 from requests import post
 from ray import remote
 from .generic_source import GenericSource
-from ..config import LIVECOINWATCH_API_KEY
 
 class Livecoinwatch(GenericSource):
     @remote
     def get_price(self, symbol:str, currency:str) -> dict or None:
         response = post(
             url=self.urls["livecoinwatch"]["price"].format(SYMBOL=symbol,CURRENCY=currency),
-            headers={"x-api-key":LIVECOINWATCH_API_KEY,"content-type":"application/json"},
+            headers={"x-api-key":"d960befc-10d2-4630-93cf-136f2e7f1558","content-type":"application/json"},
             json={"currency":currency.upper(),"code":symbol.upper(),"meta":True},
         )
         try:
