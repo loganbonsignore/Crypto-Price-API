e09f1afcf002801916bcabdc18b2ed100070ad93
Logan Bonsignore <Loganbon14@gmail.com>
2021-08-04 11:29:40 -0500

    add test_price_none

--- tests/test_api.py
+++ tests/test_api.py
@@ -9,14 +9,18 @@ def client():
 def test_health(client):
     assert client.get("/health").status_code == 200
 
-def test_price_valid(client):
+def test_price_valid_query(client):
     query = "btc_usd,eth_gbp,dot_aud,ksm_jpy"
     assert client.get(f"/price/{query}").status_code == 200
 
-def test_price_invalid_1(client):
+def test_price_invalid_query_1(client):
     query = ".btc_usd,none_Btc,do_t_eth,eth,log.an"
     assert client.get(f"/price/{query}").status_code == 200
 
-def test_price_invalid_2(client):
+def test_price_invalid_query_2(client):
     query = ",KYL_uSd,wbtc_eur,,ksm_DOT,bt,c_usd"
+    assert client.get(f"/price/{query}").status_code == 200
+
+def test_price_none(client):
+    query = "sam_usd" # returns price==None from Livecoinwatch (8/4/21)
     assert client.get(f"/price/{query}").status_code == 200
