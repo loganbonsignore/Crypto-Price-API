48a0661ac9a060ef2b476acdd6c10902eb598982
Logan Bonsignore <Loganbon14@gmail.com>
2021-08-14 07:40:39 -0500

    add livecoinwatch error handle

--- api/PriceApi.py
+++ api/PriceApi.py
@@ -9,13 +9,17 @@ from .sources.bancor import Bancor
 from .sources.kraken import Kraken
 from .sources.bitfinex import Bitfinex
 from .sources.cryptocompare import Cryptocompare
-from .sources.livecryptowatch import Livecoinwatch
+try:
+    from .sources.livecryptowatch import Livecoinwatch
+except ModuleNotFoundError:
+    print("Add LIVECOINWATCH_API_KEY = <YOUR LIVECOINWATCH API KEY> to api/sources/config.py to active Live Coin Watch as a source.")
 
 ray.init(ignore_reinit_error=True, include_dashboard=False, log_to_driver=False)
 
 class PriceApi:
     def __init__(self):
-        self.sources = [Coinbase, Coingecko, Binance, Bancor, Kraken, Bitfinex, Cryptocompare, Livecoinwatch]
+        self.sources = {Coinbase, Coingecko, Binance, Bancor, Kraken, Bitfinex, Cryptocompare}
+        if "Livecoinwatch" in globals(): self.sources.add(Livecoinwatch)
 
     def get_price_data(self, symbol:str, currency:str) -> list:
         remote_processes = [source().get_price.remote(source(), symbol, currency) for source in self.sources]
